c43bf0cdffc93322c7dbddea0875998de75de6a1
Neel07122009 <neel.kalekar@gmail.com>
2023-12-09 19:08:32 +1030

    Addition

---
+++ Practice/Add2.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Add2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num = 5;
+
+        System.out.println(num1 + num);
+    }
+}
